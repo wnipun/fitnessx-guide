1a2bdc38b846d36fa1204e67b9efbc12b40b1c31
Nipun Perera <nipun5perera@gmail.com>
2025-12-28 19:46:28 +1100

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/i18n', '@nuxt/eslint', '@nuxt/fonts'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [{
+      code: 'en',
+      name: 'English',
+    }, {
+      code: 'fr',
+      name: 'Français',
+    }],
+  },
+})
